b9f2c37e72419d9d45a887f87c1faf07e29f62d2
Sardor Urolov <sardoruralov2007@gmail.com>
2025-10-03 17:23:03 +0500

    Product List

--- main.cpp
+++ main.cpp
@@ -6,8 +6,16 @@ int main() {
 
     int Kartoshka = 8000, Piyoz = 5000, Olma=15000, Limon=25000, Pepsi = 10000;
     float Product1, Product2, Product3, Product4, Product5;
-
-    cin>>Product1>>Product2>>Product3>>Product4>>Product5;
+    cout<<"Kartoshka necha kg: ";
+    cin>>Product1;
+    cout<<"Piyoz necha kg: ";
+    cin>>Product2;
+    cout<<"Olma necha kg: ";
+    cin>>Product3;
+    cout<<"Limon necha kg: ";
+    cin>>Product4;
+    cout<<"Pepsi necha l: ";
+    cin>>Product5;
     cout<<"Kartoshka = "<<Kartoshka*Product1<<" so'm"<<endl;
     cout<<"Piyoz = "<<Piyoz*Product2<<" so'm"<<endl;
     cout<<"Olma = "<<Olma*Product3<<" so'm"<<endl;
@@ -16,6 +24,7 @@ int main() {
     int summa=Kartoshka*Product1+Piyoz*Product2+Olma*Product3+Limon*Product4+Pepsi*Product5;
     cout<<"Jami = "<<summa<<" so'm"<<endl;
     cout<<"QQS = "<<summa*0.12<<" so'm"<<endl;
+
 
 
 }
